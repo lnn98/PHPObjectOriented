e399f827ccc33f7fd29eb8309cbcca69428257c8
PC <lnn98>
2018-12-04 18:29:32 +0700

    axd

---
+++ bai1/index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Lập trình hướng đối tượng là gì?
+ * Lập trình hướng đối tượng (gọi tắt là OPP-objec oriented programming) là một kĩ thuật lập trình hỗ trợ công nghệ đối tượng nếu như trước kia là các kiểu lập trình hướng thủ tục ,hướng modun,... thì giờ đây thế giới đang ưu về sử dụng hướng đối tượng .Nếu như trước đây chúng ta lập trình với hướng thủ tục thì sẽ chia thành các hàm để xử lý thì giờ đây khi sử dụng hướng đối tượng thì chúng ta sẽ chia thành các đối tượng để xử lý
+ * VD lập trình hướng thủ tục
+ */
+function getPersonnel() {
+    $name ='Vu Van A';
+    $age = 32;
+    return $name .'-'. $age;
+}
---
+++ bai1/index1.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * lập trình hướng đối tượng OPP
+ * Ưu điểm của lập trình hướng đối tượng
+ * Vì lập trình hướng đối tượng ra đời sau nên nó khắc phục được tất cả các điểm yếu    của các phương pháp lập trình trước đó. Cụ thể các ưu điểm sau
+ * Dễ dàng quản lý code khi có sự thay đổi chương trình
+ * Dễ mở rộng dự án
+ * Tiết kiệm được tài nguyên đáng kể cho hệ thống
+ * Có tính bảo mật cao
+ * Có tính tái sử dụng cao
+ * Như vậy trong phần này mình đã nêu ra được khái niệm của lập trình hướng đối tượng và ưu điểm của phương pháp lập trình hướng đối tượng rồi, Bài tiếp theo chúng ta sẽ đi vào tìm hiểu các khái niệm về class, thuộc tính, phương thức,...
+ */
+class Personnel {
+    private $name = 'Vu Van A';
+    private $age = 32;
+    public function getPersonnel(){
+        return $this->name .'-'. $this->age;
+    }
+}

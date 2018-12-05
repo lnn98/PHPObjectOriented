c17c847c6328f5638409d242fae2823dafc0ea4f
PC <lnn98>
2018-12-05 13:05:36 +0700

    axd

--- bai7/index.php
+++ bai7/index.php
@@ -5,3 +5,23 @@
  * Date: 12/5/2018
  * Time: 12:58 PM
  */
+abstract class Dongvat
+{
+    protected $name;
+
+    abstract protected function getName();
+}
+class ConTrau extends Dongvat
+{
+    protected function getName()
+    {
+        // TODO: Implement getName() method.
+    }
+}
+class ConBo extends Dongvat
+{
+    public function getName()
+    {
+        return $this->name;
+    }
+}
